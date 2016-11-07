ae3fa560df19e96cb3a7159681407572a1d0f138
zyczzh <651625937@qq.com>
2016-11-07 10:07:52 +0800

    更新～！更新～！

--- exercise/ex42.py
+++ exercise/ex42.py
@@ -20,3 +20,34 @@ print b.add_me_up(30)
 
 print a.number
 print b.number
+
+################
+import random as r
+class Fish:
+    def __init__ (self):
+        self.x=r.randint(0,10)
+        self.y=r.randint(0,10)
+
+    def move(self):
+        self.x-=1
+        print("我的位置：",self.x,self.y)
+
+class Goldfish(Fish):
+    pass
+class Carp(Fish):
+    pass
+class Salmon(Fish):
+    pass
+class Shark(Fish):
+    def __init__(self):
+        super().__init__()
+        self.hungry=True
+        
+    def eat(self):
+        if self.hungry:
+            print("吃货的梦想是天天有得吃"）
+            self.hungry=False
+        else:
+            print("太饱了"）
+
+                    
